8e89e7b4d4e4fbdd9fcac7801e4ab3756c162cd3
MiloVirus <cami_alejo6@hotmail.com>
2024-03-25 02:02:15 -0500

    Toggle Button Animation - Mobile Nav

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,34 @@
  import "../styles/dashboard.css"
+ import { useEffect, useState } from "react"
+ import { FaBars } from "react-icons/fa";
+
+
 
 const Dashboard = () => {
+
+    const [onSwitch, setOnSwitch] = useState(false)
+
+    const switchSelection = () => {
+        setOnSwitch(!onSwitch)
+    }
+
+    const handleResize = () => {
+        if(window.innerWidth < 720)
+        { }
+        else{
+            setOnSwitch(false)
+        }
+    }
+
+    useEffect(() =>
+    {
+        window.addEventListener("resize", handleResize)
+    }, [])
+
+
   return (
     <>
-    <nav className="navDashboard">
+    <nav className={onSwitch ? "navDashboard mobile" : "navDashboard" }>
         <ul className="navDashboard__ul">
             <li className="navDashboard__li">
                 <p>Graphic</p>
@@ -16,6 +41,9 @@ const Dashboard = () => {
             </li>
         </ul>
     </nav>
+    <div>
+        <FaBars onClick={switchSelection} className="icon"/>
+    </div>
     </>
   )
 }
